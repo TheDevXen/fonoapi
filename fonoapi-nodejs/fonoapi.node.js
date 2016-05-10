0b9c21c00708d28e0d3f342033329fd27a5dac30
Trung Vo <mtrung@gmail.com>
2016-05-10 00:22:01 -0500

    finalize api + more tests

--- fonoapi-nodejs/fonoapi.node.js
+++ fonoapi-nodejs/fonoapi.node.js
@@ -1,31 +1,9 @@
 var httpreq = require('httpreq');
 
-function printHandler(queryString, data) {
-	console.log('--- \'' + queryString + '\'');
-
-	if (data.hasOwnProperty('length')) {
-		console.log('- Found ' + data.length);
-
-		data.forEach(function (device) {
-			console.log(device.DeviceName);
-		});
-	} else if (data.hasOwnProperty('message')) {
-		console.log(data.message);
-	} else {
-		console.log('Invalid data: ' + JSON.stringify(data));
-	}
-
-	console.log('---');
-}
-
-function getDevices(queryString, callback) {
-	getDevicesWithBrand(queryString, '', callback);
-}
-
-function getDevicesWithBrand(queryString, brand, callback) {
+function getDevices(callback, device, brand) {
 	var url = 'https://fonoapi.freshpixl.com/v1/getdevice';
 	var options = {
-		parameters: {device: queryString, token: module.exports.token},
+		parameters: {device: device, token: module.exports.token},
 		timeout: 2900
 	};
 
@@ -38,43 +16,68 @@ function getDevicesWithBrand(queryString, brand, callback) {
 			console.log(err);
 		} else {
 			var data = JSON.parse(res.body);
-			callback(brand + ':' + queryString, data);
+			var queryString = brand ? (brand + '.' + device) : device;
+			callback(queryString, data);
 		}
 	});
 }
 
-function getLatest(callback) {
+function getLatest(callback, limit, brand) {
 	var url = 'https://fonoapi.freshpixl.com/v1/getlatest';
 	var options = {
 		parameters: {token: module.exports.token},
 		timeout: 2900
 	};
+	if (brand) {
+		options.parameters.brand = brand;
+	}
+	if (limit) {
+		options.parameters.limit = limit;
+	}
 
 	httpreq.post(url, options, function (err, res) {
 		if (err) {
 			console.log(err);
 		} else {
 			var data = JSON.parse(res.body);
-			callback(null, data);
+			var queryString = brand ? brand : '';
+			callback(queryString, data);
 		}
 	});
 }
 
-function printDevices(queryString) {
-	getDevices(queryString, printHandler);
+function printCount(queryString, data) {
+	printData(queryString, data, false);
 }
 
-function printLatest() {
-	getLatest(printHandler);
+function printAllNames(queryString, data) {
+	printData(queryString, data, true);
+}
+
+function printData(queryString, data, printAllData) {
+	var outStr = '- "' + queryString + '"\t => ';
+
+	if (data.hasOwnProperty('length')) {
+		console.log(outStr + data.length + ' devices');
+
+		if (printAllData) {
+			data.forEach(function (device) {
+				console.log(device.DeviceName);
+			});
+			console.log('---');
+		}
+	} else if (data.hasOwnProperty('message')) {
+		console.log(outStr + data.message);
+	} else {
+		console.log(outStr + ' invalid data: ' + JSON.stringify(data));
+	}
 }
 
 module.exports = {
 	token: '',
 	getDevices: getDevices,
-	getDevicesWithBrand: getDevicesWithBrand,
 	getLatest: getLatest,
 
-	printDevices: printDevices,
-	printLatest: printLatest,
-	printHandler: printHandler
+	printCount: printCount,
+	printAllNames: printAllNames
 };
--- fonoapi-nodejs/test.js
+++ fonoapi-nodejs/test.js
@@ -1,21 +1,17 @@
 var fonoapi = require('./fonoapi.node.js');
-
-function printHandler(queryString, data) {
-	if (data.hasOwnProperty('length')) {
-		data.forEach(function (device) {
-			console.log(device.DeviceName);
-		});
-        console.log(' ');
-	} else if (data.hasOwnProperty('message')) {
-		console.log(data.message);
-	} else {
-		console.log('Invalid data: ' + JSON.stringify(data));
-	}
-}
-
 fonoapi.token = 'bb48887db376bf9d8c1b0e20ee347d9a770c811372512c8a';
-fonoapi.printDevices(' galaxy s5');
-fonoapi.printDevices('galaxy s5');
-fonoapi.getDevicesWithBrand(' galaxy s7', 'samsung', printHandler);
 
-fonoapi.printLatest();
+var testModelStrings = [
+	'galaxy s 5',	// returns empty
+	'galaxy s5',
+	'iphone', ' iphone', 'apple iphone'
+];
+
+testModelStrings.forEach(function (modelStr) {
+	fonoapi.getDevices(fonoapi.printCount, modelStr);
+});
+
+fonoapi.getDevices(fonoapi.printCount, 'iphone', 'apple');
+
+fonoapi.getLatest(fonoapi.printAllNames, 5);
+fonoapi.getLatest(fonoapi.printAllNames, 5, 'apple');
